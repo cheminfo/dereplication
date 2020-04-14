302272f8ac3be37716b68bccbc0f75440cb32515
Océane Patiny <oceane@patiny.com>
2020-04-14 17:07:09 +0200

    fix (index): add new options

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import { similarity as Similarity } from 'ml-distance';
 
 import testSimilarity from './testSimilarity';
 
-const algorithms = {
-  intersection: Similarity.intersection,
-  cosine: Similarity.cosine,
-};
+const intersection = Similarity.intersection;
+
 const experimental = './data/matchingExperiments.json';
 const predicted = './data/predictions.json';
 
 testSimilarity(experimental, predicted, {
-  numExperiments: 200,
+  numExperiments: undefined,
   alignDelta: 0.05,
   mergeSpan: 0.05,
-  algorithm: algorithms.intersection,
+  algorithm: intersection,
+  norm: 'loadData',
+  massFilter: 0.05,
 });
